55a29cf6693b545e5c9deced106438c7b89a5534
Goodness James <akomagoodness97@gmail.com>
2024-05-06 15:38:32 +0100

    Include docstring to Book class model

--- crud_app/models.py
+++ crud_app/models.py
@@ -3,6 +3,16 @@ from database import Base
 
 
 class Book(Base):
+    """
+    Represents a book in the database.
+
+    Attributes:
+        id (int): The unique identifier for the book.
+        title (str): The title of the book.
+        author (str): The author of the book.
+        year (int): The year of publication of the book.
+        isbn (str): The ISBN (International Standard Book Number) of the book.
+    """
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String)
